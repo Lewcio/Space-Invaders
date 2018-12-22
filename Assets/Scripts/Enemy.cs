6e76834d23ac75b9eea12dcc948f33d5f7450b54
Lewcio <Lewcio@users.noreply.github.com>
2018-12-22 23:14:27 +0100

    added score and win info

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
     public Transform enemies;
     public GameObject bullet;
+    public Text winText;
     private float speed;
     private int hardness;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        winText.text = "";
         speed = 0.5f;
         hardness = 95;
         InvokeRepeating("Run", 0.1f, 0.8f);
@@ -24,17 +28,32 @@ public class Enemy : MonoBehaviour {
             {
                 speed = -speed;
                 if (enemies.position.y > -2.5)
-                    enemies.position += Vector3.down * 0.3f;
+                    enemies.position += Vector3.down * 0.2f;
             }
         }
         // boss
-        if (enemies.childCount <= 1)
+        if (enemies.childCount == 1)
         {
             CancelInvoke("Run");
             InvokeRepeating("Run", 0.1f, 0.1f);
-            enemies.GetChild(0).GetComponent<Renderer>().material.color = Color.cyan;
-            print(enemies.GetChild(0).childCount);
-            hardness = 1;
+
+            foreach (Transform enemy in enemies)
+            {
+                Renderer[] rrr = enemy.GetComponents<Renderer>();
+                foreach (Renderer toChange in rrr)
+                {
+                    toChange.material.color = Color.red;
+
+                }
+            }
+            //GameObject enemy = GameObject.FindWithTag("Enemy") as GameObject;
+            //foreach(GameObject e in enemy) { }
+            //enemy.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
+            hardness = 30;
+        }
+        if (enemies.childCount == 0)
+        {
+            winText.text = "FUCK YOUR LIFE\nYOU WIN";
         }
     }
 
